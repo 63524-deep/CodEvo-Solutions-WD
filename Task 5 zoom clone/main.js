85c6ca8ebb677f2c2a96996c82a326b8523d87b9
Ghinaiya Deep <136948926+Ghinaiya-Deep@users.noreply.github.com>
2024-08-17 18:00:11 +0530

    Add files via upload
    node_modules files not upload because of size limit

---
+++ Task 5 zoom clone/main.js
@@ -0,0 +1,10 @@
+// main.js
+
+// Get the button element
+const joinButton = document.querySelector('button');
+
+// Add an event listener to the button
+joinButton.addEventListener('click', () => {
+  console.log('Join Stream button clicked!');
+  // TO DO: Implement the logic to join the stream here
+});
---
+++ Task 5 zoom clone/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a new client connected');
+
+  // Handle socket events here
+});
+
+server.listen(3001, () => {
+  console.log('Server listening on port 3001');
+});
